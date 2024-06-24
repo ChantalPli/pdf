451166841aa1644486c5dc5cb40f7244aa89fe9e
ChantalPli <chantal.pilia01@gmail.com>
2024-06-23 23:29:13 -0500

    refactor: pdf created and saved

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,9 +1,11 @@
 import { extractTextFromPDF } from '../services/pdfService.js';
 import { renameSync } from 'fs';
 import { extname } from 'path';
+import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import Document from '../models/Document.js';
 import { extractData, extractCIIU, extractObjetoSocial } from '../utils/extractors.js';
 import { Types } from 'mongoose';
+import { writeFileSync } from 'fs';
 
 export const uploadDocument = async (req, res) => {
     try {
@@ -16,6 +18,30 @@ export const uploadDocument = async (req, res) => {
 
         const fileId = new Types.ObjectId();
 
+        const dataText = `
+        Legal Representative: ${legalRepresentative}
+        Alternate Legal Representative: ${alternateLegalRepresentative}
+        Society Activity: ${societyActivity}
+        CIIU Code: ${ciiuCode}
+        `;
+
+        const pdfDoc = await PDFDocument.create()
+        const page = pdfDoc.addPage()
+
+        const fontSize = 8
+        const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+        const { width, height } = page.getSize()
+        page.drawText(dataText, {
+            x: 0,
+            y: height - 4 * fontSize,
+            size: fontSize,
+            font: timesRomanFont,
+            color: rgb(0, 0, 0),
+        })
+        const pdfBytes = await pdfDoc.save()
+
+        const newFilePath = `saved_files/${fileId}.pdf`;
+        writeFileSync(newFilePath, pdfBytes);
         const newDocument = new Document({
             legalRepresentative,
             alternateLegalRepresentative,
@@ -26,9 +52,6 @@ export const uploadDocument = async (req, res) => {
 
         await newDocument.save();
 
-        const newFilePath = `saved_files/${fileId}${extname(req.file.originalname)}`;
-        renameSync(filePath, newFilePath);
-
         res.status(200).json({
             message: 'Documento procesado y guardado exitosamente',
             fileId,
@@ -37,6 +60,8 @@ export const uploadDocument = async (req, res) => {
             societyActivity,
             ciiuCode
         });
+
+
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
